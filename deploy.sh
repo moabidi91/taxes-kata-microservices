6a9842fcf86014c76fccddf77ed217dc6a2ae694
Mohamed Oussama ABIDI <mohamed-oussama.abidi@outlook.fr>
2020-12-29 11:11:07 +0100

    finalizing project base

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-
+echo '---> Starting deploy.sh ...'
+echo 'STEP 1/3 : ---> Updating wrapper from master'
 git checkout master
 git pull origin master
 
-echo 'Update submodules'
+echo 'STEP 2/3 : ---> Updating submodules from master'
 git submodule init && git submodule sync && git submodule update
 git submodule foreach 'git checkout master && git pull origin master'
 
-echo 'Refresh NPMs modules for each submodules'
-git submodule foreach 'npm install'
-
-echo 'Docker compose up'
+echo 'STEP 3/3 : ---> Build & Run images'
 docker-compose up -d #--build 
+echo '---> End deploy.sh !'
